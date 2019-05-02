2ebc723d4c35e086011bd5d79ec05c94fdf4a908
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-05-02 11:44:53 -0400

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'kills/index'
-  get 'games/index'
-  get 'users/index'
+  # get 'kills', :as 'kills/index'
+  get '/kills' => 'kills#index', :as => 'kills'
+  get '/games' => 'games#index', :as => 'games'
+  get '/users' => 'users#index', :as => 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
