75ebd9427ebe89e09c690027c8dfc5b77657940a
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-05-07 16:33:13 -0400

    finished login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/games' => 'games#index', :as => 'games'
   get '/users' => 'users#index', :as => 'users'
   post '/kills/:id/stats' => 'kills#stats'
+  get '/users/auto_login' => 'users#auto_login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
