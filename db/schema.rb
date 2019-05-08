0c514b4c391cfe165d8842a158b0d371f32e69d6
NickyEXE <dovern42@gmail.com>
2019-05-08 11:31:27 -0400

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_190254) do
+ActiveRecord::Schema.define(version: 2019_05_08_152905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_190254) do
     t.string "password_digest"
     t.datetime "lastTimeUpdated"
     t.string "image_url"
+    t.string "email"
   end
 
 end
